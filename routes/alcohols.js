1dd14be4857cab7284d934f18893bbcfe29c8da2
pohern <ohern_p@yahoo.com>
2020-12-10 22:08:07 -0600

    add isLoggedIn function to routes

--- routes/alcohols.js
+++ routes/alcohols.js
@@ -1,6 +1,9 @@
 const router = require('express').Router()
 const alcoholsCtrl = require('../controllers/alcohols')
 
-
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/auth/google");
+}
 
 module.exports = router
--- routes/cocktails.js
+++ routes/cocktails.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const cocktailsCtrl = require('../controllers/cocktails')
 
-
-
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/auth/google");
+}
 module.exports = router
