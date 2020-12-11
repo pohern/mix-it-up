41f127f52fd9ab80a37dcbf40ea21e5b4462d1ea
pohern <ohern_p@yahoo.com>
2020-12-10 20:45:28 -0600

    add cocktail model

---
+++ models/cocktail.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const instructionSchema = new Schema({
+    glassType: {type:String, enum:['rocks','highball','martini','margarita','hurricane','shot']},
+    prepTime: Number,
+    difficulty: Number,
+    steps: [String],
+    iceCube: {type:String, enums:['cubed','large sphere','crushed','large cube']},
+})
+
+const cocktailSchema = new Schema({
+    name: String,
+    alcohols: [{type: Schema.Types.ObjectId, ref: 'Alcohol'}],
+    ingredients:[String],
+    instructions: [instructionSchema],
+    mixologist: {type:Schema.Types.ObjectId, ref: 'User'}
+})
+
+module.exports = mongoose.model('Cocktail', cocktailSchema)
